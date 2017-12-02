beb121a5c38264ec45675c9927dd041445ba9465
drinks <drinksober@foxmail.com>
2017-12-02 15:11:05 +0800

    add: add zshrc

--- config.fish
+++ config.fish
@@ -2,11 +2,20 @@ source $HOME/.config/fish/init.fish
 set PATH ~/.local/bin $PATH
 set PATH ~/.npm-global/bin $PATH
 set MANPATH ~/Documents/man:$MANPATH
+set GOPATH $HOME/go
+
+function px
+    set http_proxy 127.0.0.1:1089
+    set HTTP_PROXY 127.0.0.1:1089
+    set https_proxy 127.0.0.1:1089
+    set HTTPS_PROXY 127.0.0.1:1089
+end
 
 set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
+set -x HOMEBREW_GITHUB_API_TOKEN b975e4fc3b328b9bb8fc8cfc1c25e4cc45e51361
 
 
-export FZF_DEFAULT_COMMAND='Rg --files --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 
 alias desk 'cd ~/Desktop'
 set fish_function_path $fish_function_path "/home/linlin/.local/lib/python3.6/site-packages/powerline/bindings/fish" powerline-setup
@@ -33,8 +42,12 @@ plo = pull origin
 end
 
 function ssrd
-    cd ~/Documents/ssrd/src
-    source ~/Documents/ssrd/bin/activate.fish
+    cd ~/Documents/ssrd
+    source ~/.virtualenvs/pypy3/bin/activate.fish
+end
+
+function fe
+    cd ~/Documents/ssrd-fe
 end
 
 function ali
@@ -58,20 +71,17 @@ end
 
 function hfa
     if not count $argv > /dev/null
-        cd ~/Desktop/adtracker
-        source ~/Documents/py27/bin/activate.fish
+        cd ~/Documents/adtracker
+        source ~/.virtualenvs/py27/bin/activate.fish
     else
         switch $argv
             case old
                 cd ~/Desktop/hfa/new_adtracker
-                source ~/Documents/py27old/bin/activate.fish
+                source ~/.virtualenvs/py27old/bin/activate.fish
             case jmp
                 sshpass -p drinks ssh drinks_huang@$JMP -p 31122
             case dev
                 sshpass -p redhat ssh root@10.123.1.119 -p 2279
-            case pypy
-                cd ~/Desktop/forest
-                source ~/Documents/pypy/bin/activate.fish
             case cas
                 sshpass -p redhat  ssh root@10.123.1.119
             case jks
@@ -82,16 +92,16 @@ function hfa
                 sshpass -p redhat ssh root@10.23.46.174
 
             case '*'
-                cd ~/Desktop/adtracker
-                source ~/Documents/py27/bin/activate.fish
+                cd ~/Documents/adtracker
+                source ~/.virtualenvs/py27/bin/activate.fish
         end
     end
 end
 
 function board
     if not count $argv > /dev/null
-        cd ~/Desktop/Dashboard
-        source ~/Documents/py36/bin/activate.fish
+        cd ~/Documents/Dashboard
+        source ~/.virtualenvs/py36/bin/activate.fish
     else
         switch $argv
             case 'dev'
@@ -99,8 +109,8 @@ function board
             case cas
                 sshpass -p redhat  ssh root@10.123.1.119
             case '*'
-                cd ~/Desktop/adtracker
-                source ~/Documents/py27/bin/activate.fish
+                cd ~/Documents/adtracker
+                source ~/.virtualenvs/py36/bin/activate.fish
         end
     end
 end
@@ -109,6 +119,10 @@ function jsonpp
     echo $argv[1] | python -m json.tool
 end
 
+function dl
+    set $cmd "wget $argv"
+    echo $cmd
+end
 function updateall
     echo 'start updating ...'
 
@@ -123,6 +137,8 @@ function updateall
     echo 'checking Apple Updates'
     /usr/sbin/softwareupdate -ia
 end
+
+    
 
 
 #test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
